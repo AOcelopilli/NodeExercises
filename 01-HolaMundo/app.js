066148b89b11548b229f4114395f5d951fdd1e4a
angelp495 <angeljpa95@gmail.com>
2021-02-23 10:43:47 -0600

    Hola Mundo

---
+++ 01-HolaMundo/app.js
@@ -0,0 +1,14 @@
+console.log("Hola Mundo");
+
+/* let nombre = "Angel";
+console.log(nombre);
+
+nombre = "Jesus";
+console.log(nombre);
+ */
+
+const nombre = "Angel";
+console.log(nombre);
+
+const nombre2 = "Jesus";
+console.log(nombre2);
