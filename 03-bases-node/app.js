5e930effd022520a0106a86bed4876ad8d015bee
angelp495 <angeljpa95@gmail.com>
2021-05-29 10:42:51 -0500

    uso del paquete colors

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,10 @@
 const { crearArchivo } = require("./helpers/multiplicar");
-
 const yargs = require("./config/yargs");
+const colors = require("colors");
 
 console.clear();
 console.log(yargs);
 
 crearArchivo(yargs.b, yargs.l)
   .then((nombreArchivo) => console.log(nombreArchivo, `creado`))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err.brightRed));
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,31 +1,25 @@
 /* Requerir paquetes se hace con la palabra reservada 'require' */
 const fs = require("fs");
+const colors = require("colors");
 
 const crearArchivo = async (base = 5, listar = false) => {
   try {
     let salida = "";
 
     for (let i = 1; i < 11; i++) {
-      salida += `${base} x ${i} = ${base * i}\n`;
+      salida += `${base} x ${i} = ${base * i}\n`.random;
     }
 
     if (listar == true) {
-      console.log("=======================");
-      console.log(`     Tabla del: `, base);
-      console.log("=======================");
+      console.log("=======================".rainbow);
+      console.log(`     Tabla del: `.bold, colors.blue(base));
+      console.log("=======================".rainbow);
       console.log(salida);
     }
 
-    /* 
-    fs.writeFile('tabla-5.txt', salida, (err) => {
-      if (err) throw err;
-      console.log('tabla-5.txt creado')
-    });
-    */
-
     fs.writeFileSync(`tabla-${base}.txt`, salida);
 
-    return `tabla-${base}.txt`;
+    return `tabla-${base}.txt`.green;
   } catch (err) {
     throw err;
   }
