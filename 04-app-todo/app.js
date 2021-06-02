60ad19ae65464e8fe4ccc232a5ebf1da32a7984c
angelp495 <angeljpa95@gmail.com>
2021-06-02 12:21:28 -0500

    Mostrar tareas completadas e imcompletas

--- 04-app-todo/app.js
+++ 04-app-todo/app.js
@@ -1,5 +1,6 @@
 require("colors");
 const inquirer = require("inquirer");
+const { guardarDB, leerDB } = require("./helpers/guardarArchivo");
 const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
 const Tarea = require("./models/tarea");
 const Tareas = require("./models/tareas");
@@ -10,19 +11,40 @@ const main = async () => {
   let opt = "";
   const tareas = new Tareas();
 
+  const tareasDB = leerDB();
+
+  if (tareasDB) {
+    // Establecer las tareas
+    tareas.cargarTareasFromArray(tareasDB);
+  }
+
   do {
+    // Imprimir el menu
     opt = await inquirerMenu();
 
+    // Manejar la opcion recibida.
     switch (opt) {
       case "1":
         //crear opcion
-        const desc = await leerInput("descriptcion: ");
+        const desc = await leerInput("Descriptcion: ");
         tareas.crearTarea(desc);
         break;
       case "2":
-        console.log(tareas._listado);
+        // console.log(tareas.listadoArr);
+        tareas.listadoCompleto();
+        break;
+      case "3":
+        // console.log(tareas.listadoArr);
+        tareas.listarPendientesCompletadas(true);
+        break;
+
+      case "4":
+        // console.log(tareas.listadoArr);
+        tareas.listarPendientesCompletadas(false);
         break;
     }
+
+    guardarDB(tareas.listadoArr);
 
     await pausa();
   } while (opt !== "0");
--- 04-app-todo/models/tareas.js
+++ 04-app-todo/models/tareas.js
@@ -3,8 +3,29 @@ const Tarea = require("./tarea");
 class Tareas {
   _listado = {};
 
+  get listadoArr() {
+    const listado = [];
+    Object.keys(this._listado).forEach((key) => {
+      // Con ayuda de la KEY podemos acceder a la tarea completa y crear un arreglo.
+      const tarea = this._listado[key];
+      listado.push(tarea);
+    });
+
+    listado.push();
+
+    return listado;
+  }
+
   constructor() {
     this._listado = {};
+  }
+
+  cargarTareasFromArray(tareas = []) {
+    // mostrar tareas
+
+    tareas.forEach((tarea) => {
+      this._listado[tarea.id] = tarea;
+    });
   }
 
   crearTarea(desc = "") {
@@ -12,6 +33,53 @@ class Tareas {
 
     this._listado[tarea.id] = tarea;
   }
+
+  listadoCompleto() {
+    // 1: en verde
+    // Completada: verde
+    // Pendiente: rojo
+    // 1. tarea
+    console.log();
+
+    this.listadoArr.forEach(({ id, desc, completadoEn: estado }, idx) => {
+      let indice = idx + 1;
+
+      console.log(
+        `${indice}.`.green,
+        desc,
+        `${estado === null ? "Pendiente".red : "Completado".green}`
+      );
+    });
+
+    console.log();
+  }
+
+  listarPendientesCompletadas(completadas = false) {
+    let tareasParaMostrar = [];
+
+    if (!completadas) {
+      tareasParaMostrar = this.listadoArr.filter(
+        (tarea) => tarea.completadoEn === null
+      );
+    } else {
+      tareasParaMostrar = this.listadoArr.filter(
+        (tarea) => tarea.completadoEn !== null
+      );
+    }
+
+    formatearTareas(tareasParaMostrar);
+  }
 }
+
+const formatearTareas = (tareas) => {
+  console.log();
+  tareas.forEach(({ id, desc, completadoEn }, idx) => {
+    let indice = `${idx + 1}.`.green,
+      estado = completadoEn ? "Completo".green : "Pendiente".red;
+
+    console.log(`${indice} ${desc} :: ${completadoEn}`);
+  });
+  console.log();
+};
 
 module.exports = Tareas;
