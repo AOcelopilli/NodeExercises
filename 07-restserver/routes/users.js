c4e5a605671ee467e34a7a202dc5c82abf1476c7
Angel_Pineda <angeljpa95@gmail.com>
2021-12-09 18:22:16 -0600

    07 - validar campos vs db

--- 07-restserver/routes/users.js
+++ 07-restserver/routes/users.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
+const Role = require("../models/role");
+
 const { validarCampos } = require("../middlewares/validar-campos");
 const {
   usersGet,
@@ -22,7 +24,16 @@ router.post(
       min: 6,
     }),
     check("correo", "El correo no es valido").isEmail(),
-    check("rol", "No es un rol valido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    //check("rol", "No es un rol valido").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    check("rol").custom(async (rol = "") => {
+      const isRole = await Role.findOne({ rol });
+
+      if (!isRole) {
+        throw new Error(
+          `El rol ${rol} no esta registrado en la base de datos.`
+        );
+      }
+    }),
     validarCampos,
   ],
   usersPost
