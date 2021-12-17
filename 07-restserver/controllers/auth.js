ea454d797c547851d3897096122e396a3be189e3
Angel_Pineda <angeljpa95@gmail.com>
2021-12-17 12:16:00 -0600

    07 - validacion de usuario

--- 07-restserver/controllers/auth.js
+++ 07-restserver/controllers/auth.js
@@ -1,9 +1,49 @@
-const { response } = require("express");
+const { response, request } = require("express");
+const Usuario = require("../models/user");
+const bcrypt = require("bcrypt");
 
-const login = (req, res = response) => {
-  res.json({
-    msg: "Login ok",
-  });
+const login = async (req = request, res = response) => {
+  const { correo, password } = req.body;
+
+  try {
+    // verificar si el email existe
+
+    const usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Correo / Contraseña no son correctos - correo",
+      });
+    }
+
+    // el usuario esta activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Correo / Contraseña no son correctos - estado: false",
+      });
+    }
+    // verificar la contrasenia
+    const validPassword = bcrypt.compareSync(password, usuario.password);
+
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Correo / Contraseña no son correctos - password",
+      });
+    }
+    // generar el jwt
+
+    res.json({
+      msg: "Login ok",
+      correo,
+      password,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      msg: "Hable con el administrador.",
+    });
+  }
 };
 
 module.exports = {
