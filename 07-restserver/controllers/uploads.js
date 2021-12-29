7ef1bcc1067f70806cf2bca5ff148520ca8e90ce
Angel_Pineda <angeljpa95@gmail.com>
2021-12-29 13:18:00 -0600

    07 - carga de imagenes a cloudinary

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,3 +1,9 @@
+const path = require("path");
+const fs = require("fs");
+
+const cloudinary = require("cloudinary").v2;
+cloudinary.config(process.env.CLOUDINARY_URL);
+
 const { response } = require("express");
 const { Usuario, Producto } = require("../models");
 const { subirArchivo } = require("../helpers");
@@ -48,6 +54,16 @@ const actualizarImagen = async (req, res = response) => {
       });
   }
 
+  // Limpiar imagenes previas.
+  if (modelo.img) {
+    // Borrar img del servidor.
+    const pathImg = path.join(__dirname, "../uploads", coleccion, modelo.img);
+
+    if (fs.existsSync(pathImg)) {
+      fs.unlinkSync(pathImg);
+    }
+  }
+
   const nombre = await subirArchivo(req.files, undefined, coleccion);
 
   modelo.img = nombre;
@@ -57,7 +73,113 @@ const actualizarImagen = async (req, res = response) => {
   res.json(modelo);
 };
 
+const actualizarImagenCloudinary = async (req, res = response) => {
+  const { coleccion, id } = req.params;
+
+  let modelo;
+
+  switch (coleccion) {
+    case "usuarios":
+      modelo = await Usuario.findById(id);
+
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`,
+        });
+      }
+
+      break;
+
+    case "productos":
+      modelo = await Producto.findById(id);
+
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`,
+        });
+      }
+
+      break;
+
+    default:
+      return res.status(500).json({
+        msg: "Opción no valida por el momento.",
+      });
+  }
+
+  // Limpiar imagenes previas.
+  if (modelo.img) {
+    // Borrar img del servidor.
+    const nombreArr = modelo.img.split("/");
+    const nombre = nombreArr[nombreArr.length - 1];
+    const [public_id] = nombre.split(".");
+
+    cloudinary.uploader.destroy(public_id);
+  }
+
+  const { tempFilePath } = req.files.archivo;
+  const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+
+  modelo.img = secure_url;
+
+  await modelo.save();
+
+  res.json(modelo);
+};
+
+const mostrarImagen = async (req, res = response) => {
+  const pathImgPlaceholder = path.join(__dirname, "../assets/no-image.jpg");
+
+  const { coleccion, id } = req.params;
+
+  let modelo;
+
+  switch (coleccion) {
+    case "usuarios":
+      modelo = await Usuario.findById(id);
+
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`,
+        });
+      }
+
+      break;
+
+    case "productos":
+      modelo = await Producto.findById(id);
+
+      if (!modelo) {
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`,
+        });
+      }
+
+      break;
+
+    default:
+      return res.status(500).json({
+        msg: "Opción no valida por el momento.",
+      });
+  }
+
+  // Limpiar imagenes previas.
+  if (modelo.img) {
+    // Borrar img del servidor.
+    const pathImg = path.join(__dirname, "../uploads", coleccion, modelo.img);
+
+    if (fs.existsSync(pathImg)) {
+      return res.sendFile(pathImg);
+    }
+  }
+
+  // imagen placeholder
+  res.sendFile(pathImgPlaceholder);
+};
+
 module.exports = {
   cargarArchivo,
   actualizarImagen,
+  mostrarImagen,
+  actualizarImagenCloudinary,
 };
--- 07-restserver/routes/uploads.js
+++ 07-restserver/routes/uploads.js
@@ -1,12 +1,29 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
-const { cargarArchivo, actualizarImagen } = require("../controllers/uploads");
+const {
+  cargarArchivo,
+  actualizarImagen,
+  mostrarImagen,
+  actualizarImagenCloudinary,
+} = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
 const { validarArchivoSubir } = require("../middlewares");
 
 const { validarCampos } = require("../middlewares/validar-campos");
 
 const router = Router();
+
+router.get(
+  "/:coleccion/:id",
+  [
+    check("id", "El ID debe ser de Mongo").isMongoId(),
+    check("coleccion").custom((c) =>
+      coleccionesPermitidas(c, ["usuarios", "productos"])
+    ),
+    validarCampos,
+  ],
+  mostrarImagen
+);
 
 router.post("/", validarArchivoSubir, cargarArchivo);
 
@@ -20,7 +37,7 @@ router.put(
     ),
     validarCampos,
   ],
-  actualizarImagen
+  actualizarImagenCloudinary
 );
 
 module.exports = router;
