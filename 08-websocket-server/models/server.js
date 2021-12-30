ca80244de834efbd88a94eff2a9cacfee09ec5c1
Angel_Pineda <angeljpa95@gmail.com>
2021-12-30 15:15:30 -0600

    08 - bases de sockets

--- 08-websocket-server/models/server.js
+++ 08-websocket-server/models/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { socketController } = require("../sockets/controller");
 
 class Server {
   constructor() {
@@ -33,23 +34,7 @@ class Server {
   }
 
   sockets() {
-    this.io.on("connection", (socket) => {
-      //console.log("Cliente conectado");
-
-      socket.on("disconnect", () => {
-        //console.log("Cliente desconectado", socket.id);
-      });
-
-      // con socket.on escuchamos lo que es enviado
-      socket.on("enviar-mensaje", (payload, callback) => {
-        // con esto enviamos mensaje a todos los clietnes
-        //this.io.emit("enviar-mensaje", payload.mensaje);
-
-        const id = 123456;
-
-        callback(id);
-      });
-    });
+    this.io.on("connection", socketController);
   }
 
   listen() {
--- 08-websocket-server/sockets/controller.js
+++ 08-websocket-server/sockets/controller.js
@@ -1,4 +1,21 @@
-const socketController = () => {};
+const socketController = (socket) => {
+  console.log("Cliente conectado", socket.id);
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado", socket.id);
+  });
+
+  // con socket.on escuchamos lo que es enviado
+  socket.on("enviar-mensaje", (payload, callback) => {
+    // con esto enviamos mensaje a todos los clietnes
+
+    const id = 123456;
+
+    callback(id);
+
+    socket.broadcast.emit("enviar-mensaje", payload.mensaje);
+  });
+};
 
 module.exports = {
   socketController,
