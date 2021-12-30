34b6d18fd65422a0fcfa0d178aba99f8c764e8e2
Angel_Pineda <angeljpa95@gmail.com>
2021-12-30 13:26:39 -0600

    08 - config del web socket

---
+++ 08-websocket-server/models/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.server = require("http").createServer(this.app);
+    // io - es toda la información de los sockets conectados
+    this.io = require("socket.io")(this.server);
+
+    this.paths = {};
+
+    // Middlewares
+    this.middlewares();
+
+    // Rutas de mi aplicacion
+    this.routes();
+  }
+
+  middlewares() {
+    // cors
+    this.app.use(cors());
+    // directorio publico
+    this.app.use(express.static("public"));
+  }
+
+  routes() {
+    //this.app.use(this.paths.auth, require("../routes/auth"));
+  }
+
+  listen() {
+    this.server.listen(this.port, () => {
+      console.log(`Corriendo en el puerto ${this.port}`);
+    });
+  }
+}
+
+module.exports = Server;
